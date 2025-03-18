989708aa1fe50c227a4c79a3f57d7e8ea701caac
heypolygot <helloajaydey@gmail.com>
2025-03-18 22:14:51 +0530

    some bugs fixed

--- index.js
+++ index.js
@@ -28,6 +28,33 @@ function filterTask(term) {
 }
 
 
+function saveTasks(){
+    const taskItems = Array.from(tasks.children).map(task => task.querySelector('span').textContent);
+    localStorage.setItem('tasks', JSON.stringify(taskItems));
+}
+
+
+function loadTasks() {
+    const taskItems = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.innerHTML = "";
+    taskItems.forEach(task => {
+        tasks.innerHTML += `<li>
+                            <span>${task}</span>
+                            <i class="bi bi-trash3-fill delete"></i>
+                        </li>`;
+    });
+}
+
+
+function initialiseTasks() {
+    if(!localStorage.getItem('tasks')){
+        const predefinedTasks = ['Coding', 'Gaming', 'Sleeping'];
+        localStorage.setItem('tasks', JSON.stringify(predefinedTasks));
+    }
+    loadTasks();
+    updateMessage();   
+}
+
 
 addForm.addEventListener('submit', event => {
     event.preventDefault();
@@ -38,6 +65,7 @@ addForm.addEventListener('submit', event => {
                             <i class="bi bi-trash3-fill delete"></i>
                         </li>`;
         addForm.reset();
+        saveTasks();
     }
     updateMessage();
 });
@@ -48,6 +76,7 @@ tasks.addEventListener('click', event =>{
     if(event.target.classList.contains("delete")){
         console.log(event.target);
         event.target.parentElement.remove();
+        saveTasks();
     }
     updateMessage();
 
@@ -58,6 +87,7 @@ clearAll.addEventListener("click", event => {
     const taskItems = tasks.querySelectorAll("li");
     taskItems.forEach(item => {
         item.remove();
+        saveTasks();
     });
     updateMessage();
 });
@@ -77,3 +107,6 @@ searchForm.addEventListener("click", event =>{
         filterTask(term);
     }
 });
+
+
+document.addEventListener("DOMContentLoaded", initialiseTasks);
